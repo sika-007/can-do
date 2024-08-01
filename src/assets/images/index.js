f51459edec0b322ae7ed333dbd9037c5d30d01c1
Nsikak Thomas <nsikakthomas102@gmail.com>
2024-08-02 00:56:47 +0100

    Removed Design Helpers

--- src/assets/images/index.js
+++
@@ -1,4 +0,0 @@
-export { default as bgDesktopDark } from "./bg-desktop-dark.jpg";
-export { default as bgDesktopLight } from "./bg-desktop-light.jpg";
-export { default as bgMobileDark } from "./bg-desktop-dark.jpg";
-export { default as bgMobileLight } from "./bg-desktop-light.jpg";
