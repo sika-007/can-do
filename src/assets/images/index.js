2214b432351c9f9f7cf371f1ea80de54be214a27
Nsikak Thomas <nsikakthomas102@gmail.com>
2024-08-02 01:46:28 +0100

    Application completed with base features

---
+++ src/assets/images/index.js
@@ -0,0 +1,1 @@
+export { default as bgDesktopDark } from "./bg-desktop-dark.jpg";
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,7 +6,11 @@ const Background = () => {
       <div className="w-full">
         <img className="w-full" src={bgDesktopDark} alt="top-bg" />
       </div>
-      <div className="bg-lightTheme-very-light-gray dark:bg-darkTheme-very-dark-blue w-full h-full transition-all" />
+      <div className="bg-lightTheme-very-light-gray dark:bg-darkTheme-very-dark-blue w-full h-full transition-all flex items-center justify-center">
+        <p className="absolute bottom-28 text-black dark:text-white">
+          Drag and drop to reorder your Candos
+        </p>
+      </div>
     </div>
   );
 };
